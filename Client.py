774b0efd464b3b04c2149a00045256a82b563405
Ivadia Elmina Patola <ivadia2401@gmail.com>
2015-09-22 11:25:25 +0700

    Penambahan code validasi entri IP address dan port (Server dan Client)

--- Client.py
+++ Client.py
@@ -3,27 +3,37 @@ import socket
  
 #ukuran buffer ketika menerima pesan
 SIZE = 1024
- 
-#membuat objek socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = raw_input("Masukkan IP Server: ")
-server_port = input("Masukkan Port Server: ")
+try :
+    #membuat objek socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except :
+    print "Tidak dapat membuat socket..."
+else :
+    print "Socket berhasil dibuat..."
+    while 1 :
+        try :
+            server_address = raw_input("Masukkan IP Server: ")
+            server_port = input("Masukkan Port Server: ")
+
+            #koneksi server
+            s.connect((server_address,server_port))
+        except :
+            print "Error koneksi ke server..."
+        else :
+            print "Terhubung dengan server:", server_address, "pada port:", server_port
+            while 1:
+             pesan = raw_input("Pesan: ")
+             if not pesan : break
  
-#koneksi server
-s.connect((server_address,server_port))
+             #mengirim pesan ke server
+             s.send(pesan)
  
-while 1:
- pesan = raw_input("Pesan: ")
- if not pesan : break
- 
- #mengirim pesan ke server
- s.send(pesan)
- 
- #menerima pesan dari server
- try:
-     message = s.recv(SIZE)
-     print message
- except:
-     if not message : break
-s.close()
+             #menerima pesan dari server
+             try:
+                 message = s.recv(SIZE)
+                 print message
+             except:
+                 if not message : break
+    #menutup socket
+    s.close()
